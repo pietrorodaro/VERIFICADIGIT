573ce6ac8df5546b74a3e1b300678ac6e08e2171
pietro.rodaro <pietro.rodaro@PC0119BIS>
2025-03-03 09:05:14 +0100

    Implementazione del file Libro.cs

--- WpfAppVERIFICADIGIT/WpfAppVERIFICADIGIT/Libro.cs
+++ WpfAppVERIFICADIGIT/WpfAppVERIFICADIGIT/Libro.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace WpfAppVERIFICADIGIT
 {
-    internal class Libro
+    public class Libro
     {
+        private string casa_editrice;
+        private int lugnhezza;
+        private string titolo;
+
+        public string Casa_editrice { get { return casa_editrice; } set { casa_editrice = value; } }
+        public int Lugnhezza { get { return lugnhezza; } set { lugnhezza = value; } }
+        public string Titolo { get { return titolo; } set { titolo = value; } }
+
+        public Libro(string casa_editrice, int lunghezza, string titolo)
+        {
+            this.casa_editrice = casa_editrice;
+            this.lugnhezza = lunghezza;
+            this.titolo = titolo;
+        }
+
     }
 }
