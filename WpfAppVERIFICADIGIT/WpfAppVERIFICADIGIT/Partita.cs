39dab6243036568391f6e385b400abdd34e48860
pietro.rodaro <pietro.rodaro@PC0119BIS>
2025-03-03 09:10:55 +0100

    Implementazione file partita.cs

--- WpfAppVERIFICADIGIT/WpfAppVERIFICADIGIT/Partita.cs
+++ WpfAppVERIFICADIGIT/WpfAppVERIFICADIGIT/Partita.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace WpfAppVERIFICADIGIT
 {
-    internal class Partita
+    public class Partita
     {
+        private string squadra_casa;
+        private string squadra_trasferta;
+        private int anno;
+        
+
+        public string Squadra_casa { get { return squadra_casa; } set { squadra_casa = value; } }
+        public int Lugnhezza { get { return anno; } set { anno = value; } }
+        public string Squadra_trasferta { get { return squadra_trasferta; } set { squadra_trasferta = value; } }
+
+        public Partita(string squdra_casa,  string squadra_trasferta, int anno)
+        {
+            this.squadra_casa = squdra_casa;
+            this.squadra_trasferta= squadra_trasferta;
+            this.anno = anno;
+        }
+
     }
 }
